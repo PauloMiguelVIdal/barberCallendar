6c03e4d6bd1325f1d6325eedcbbe3d0e56daae77
PauloMiguelVidal <paulo2.0miguel@gmail.com>
2026-05-20 13:22:35 -0300

    realizado ajustes ui

--- app/components/Callendar.tsx
+++ app/components/Callendar.tsx
@@ -5,11 +5,35 @@ import { useState } from "react"
 import Horario from "./Horario";
 import Formulario from "./Formulario";
 import { horarioType } from '../types/horario'
+import { ArrowBigLeft, ArrowBigRight} from 'lucide-react';
 
 
 
 export default function Callendar() {
 
+  const esseAno = new Date().getFullYear()
+  const esseMês = new Date().getMonth()
+  const esseDia = new Date().getDay()
+  const esseMêsDays = new Date(esseAno, esseMês, 0).getDate()
+
+  const [diaVizualizado, setDiaVizualizado] = useState(esseDia)
+  const [mesVizualizado, setMesVizualizado] = useState(esseMês)
+
+  function setProximoDia() {
+    if (esseMêsDays === diaVizualizado) {
+      console.log(esseMês)
+      setDiaVizualizado(1)
+      setMesVizualizado(mesVizualizado + 1)
+    }else{
+      setDiaVizualizado(diaVizualizado + 1)
+    }
+
+  }
+
+  function setAnteriorDia() {
+
+    setDiaVizualizado(diaVizualizado - 1)
+  }
 
 
   const [horarios, setHorarios] = useState<horarioType[]>([
@@ -56,20 +80,29 @@ export default function Callendar() {
   }
 
   return (
-    <div className="flex items-center flex-col">
+    <div className="w-full h-full"> 
+      <div className="w-full h-[80px] bg-white fixed flex items-center justify-self-center justify-around self-center">
+        <button onClick={setAnteriorDia} disabled={esseDia === diaVizualizado} className={`flex w-[50px] h-[50px] ${esseDia === diaVizualizado ? 'bg-black/30' : 'bg-black'} rounded-full items-center justify-center`}><ArrowBigLeft /></button>
+        {/* <h1 className="text-[40px] text- text-black">{diaVizualizado}/{esseMês}</h1>/ */}
+<h1 className="absolute left-1/2 -translate-x-1/2 text-[40px] text-black">
+  15/5
+</h1>        <button onClick={setProximoDia} className="flex w-[50px] h-[50px] bg-black rounded-full items-center justify-center"><ArrowBigRight /></button>
+      </div>
+      <div className="flex items-center flex-col">
 
-      {horarios.map((horario) => (
-        <Horario
-        key={horario.hora}
-        horario={horario}
-        onSelecionar={(horario) => {
-          
-            setHorarioselecionado(horario),
-              setModalAgendamento(true)
-          }}
-        />
-      ))}
-      {modalAgendamento && horarioSelecionado && <Formulario horario={horarioSelecionado.hora} fecharModal={() => setModalAgendamento(false)} agendarHorario={agendarHorario} />}
+        {horarios.map((horario) => (
+          <Horario
+            key={horario.hora}
+            horario={horario}
+            onSelecionar={(horario) => {
+
+              setHorarioselecionado(horario),
+                setModalAgendamento(true)
+            }}
+          />
+        ))}
+        {modalAgendamento && horarioSelecionado && <Formulario horario={horarioSelecionado.hora} fecharModal={() => setModalAgendamento(false)} agendarHorario={agendarHorario} />}
+      </div>
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -7,62 +7,51 @@ import CallendarMonth from "./components/CallendarMonth";
 import { useState } from "react";
 import CallendarWeek from "./components/CallendarWeek";
 export default function Home() {
-  const esseAno = new Date().getFullYear()
-  const esseMês = new Date().getMonth()
-  const esseDia = new Date().getDay()
-  const esseMêsDays = new Date(esseAno, esseMês, 0).getDate()
 
-  const [diaVizualizado, setDiaVizualizado] = useState(esseDia)
-  const [mesVizualizado, setMesVizualizado] = useState(esseMês)
 
-  function setProximoDia() {
-    if (esseMêsDays === diaVizualizado) {
-      console.log(esseMês)
-      setDiaVizualizado(1)
-      setMesVizualizado(mesVizualizado + 1)
-    }else{
-      setDiaVizualizado(diaVizualizado + 1)
-    }
 
-  }
+ const [interfaceView,setInterfaceView] = useState('day')
 
-  function setAnteriorDia() {
 
-    setDiaVizualizado(diaVizualizado - 1)
-  }
+
+ 
+
+
+
+
+
 
 
   return (
     <div className="h-screen flex flex-col justify-center bg-zinc-50">
       <header>
         <div className="h-[100px] border-b bg-black">
-          <h1 className=" text-center text-2xl p-4">
-            BARBEARIA
+          <h1 className=" text-center text-[40px] p-4">
+            BRAVE BOSS
           </h1>
         </div>
-        <div className="w-full h-[80px] bg-white fixed flex items-center justify-around self-center">
+        {/* <div className="w-full h-[80px] bg-white fixed flex items-center justify-around self-center">
           <button onClick={setAnteriorDia} disabled={esseDia === diaVizualizado} className={`flex w-[50px] h-[50px] ${esseDia === diaVizualizado ? 'bg-black/30' : 'bg-black'} rounded-full items-center justify-center`}><ArrowBigLeft /></button>
           <h1 className="text-[40px] text- text-black">{diaVizualizado}/{esseMês}</h1>/
           <button onClick={setProximoDia} className="flex w-[50px] h-[50px] bg-black rounded-full items-center justify-center"><ArrowBigRight /></button>
-        </div>
+        </div> */}
       </header>
       <main className="flex-1 overflow-y-auto p-8">
-        {/* <Callendar /> */}
-        {/* <CallendarMonth /> */}
-
-        <CallendarWeek/>
+      {interfaceView === 'day' && ( <Callendar />)}
+      {interfaceView === 'week' && ( < CallendarWeek/>)}
+      {interfaceView === 'month' && ( <CallendarMonth />)}
       </main>
       <footer className="border-t bg-black p-4 flex items-center justify-center">
         <div className="flex w-[90%] justify-center">
           <div className="flex w-[60%] justify-between">
 
-            <button className={`flex justify-center rounded-xl border-solid border-white border-1 items-center w-[50px] aspect-square bg-linear-to-br from-black to-[#D3AF37]`}>
+            <button onClick={()=>setInterfaceView('day')} className={`flex justify-center rounded-xl border-solid border-white border-1 items-center w-[50px] aspect-square ${interfaceView === 'day'?`bg-linear-to-br from-black to-[#D3AF37]`:`bg-linear-to-br from-black to-white/40`}`}>
               <Calendar1 />
             </button>
-            <button className="flex justify-center rounded-xl border-solid border-white border-1 items-center w-[50px] aspect-square  bg-linear-to-br from-black to-white/40">
+            <button onClick={()=>setInterfaceView('week')} className={`flex justify-center rounded-xl border-solid border-white border-1 items-center w-[50px] aspect-square  ${interfaceView === 'week'?`bg-linear-to-br from-black to-[#D3AF37]`:`bg-linear-to-br from-black to-white/40`}`}>
               <Calendar />
             </button>
-            <button className="flex justify-center rounded-xl border-solid border-white border-1 items-center w-[50px] aspect-square  bg-linear-to-br from-black to-white/40">
+            <button onClick={()=>setInterfaceView('month')} className={`flex justify-center rounded-xl border-solid border-white border-1 items-center w-[50px] aspect-square ${interfaceView === 'month'?`bg-linear-to-br from-black to-[#D3AF37]`:`bg-linear-to-br from-black to-white/40`}`}>
               <CalendarDays />
             </button>
           </div>
